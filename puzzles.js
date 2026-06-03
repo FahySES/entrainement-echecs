79747ff0c3928e97cb37f8f627e70e81ce1cb6bc
FahySES <bertrand.fahys@yahoo.fr>
2026-06-03 17:11:11 +0200

    Update puzzles.js

--- puzzles.js
+++ puzzles.js
@@ -171,5 +171,15 @@ indice: "Le cavalier profite d’un clouage sur la colonne h."
   explication: "Le sacrifice de dame permet d'attirer le roi.",
   indice: "La dame peut se sacrifier pour attirer le roi noir sur une case fatale."
 },
+  {
+  id: "mat3-001",
+  categorie: "mate3",
+  theme: "Mat en 3",
+  niveau: "Guidé",
+  fen: "5rk1/5ppp/2BQ4/6n1/2PR4/5p1P/P5PK/4q3 w - - 0 1",
+  solution: ["d6f8", "g8f8", "d4d8", "e1e8", "d8e8"],
+  explication: "La dame se sacrifie pour attirer le roi noir, puis la tour force l'interposition de la dame avant de mater.",
+  indice: "Commence par attirer le roi noir sur f8."
+},
 ];
 
